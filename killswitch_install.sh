c05c6c6f5a3f4287f530153f4961d96678f0039c
MorningCoffeeZombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2018-10-26 19:31:49 -0500

    Install hosts file

--- killswitch_install.sh
+++ killswitch_install.sh
@@ -4,8 +4,19 @@
 # Set common variables
 DISTRO="unknown"
 VPNHOST="nord"
+TUNNEL=tun0
 TODAYISO=`date '+%Y%m%d-%H%M'`
 
+
+# Offer a more secure hosts file
+while true; do
+	read -p "Install a new hosts file to increase security and prevent ads? (y/n) " yn
+	case $yn in
+		[Yy]* ) INSTALLHOSTS="install"; break;;
+		[Nn]* ) INSTALLHOSTS="skip"; break;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done		
 
 # Make package management agnostic
 # Supporting Ubuntu + derivatives, Mint, Pure, Kali, Parrot and Tails
@@ -27,7 +38,30 @@ fi
 # Setting terminal commands/service
 # Saving known VPNs
 # Finding fastest VPN
-# Saving original host file as a .BAK with today's date in ISO format
+
+
+# Saving original host file as a .BAK with today's date in ISO format and then installing modified verson
+if [ $INSTALLHOSTS = "install" ]; then
+	sudo cp /etc/hosts /etc/hosts.BAK$TODAYISO
+	if [ $DISTRO = "solus" ]; then
+		sudo cp solus_hosts /etc/hosts
+	elif [ $DISTRO = "debian" ]; then
+		sudo cp debian_hosts /etc/hosts
+	fi
+fi
+
+
+
+
+
+##################
+# UNUSED RESOURCES
+##################
+
+
+
+
+
 
 
 
