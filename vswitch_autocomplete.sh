39bf7e05de07f60afbf60af5b403f43f2257e4f7
morningcoffeezombie <40343762+MorningCoffeeZombie@users.noreply.github.com>
2018-11-19 20:57:14 -0600

    Server recommender can now recommend, need to make it useful. Region may be set to 'clear'

--- vswitch_autocomplete.sh
+++ vswitch_autocomplete.sh
@@ -4,7 +4,7 @@ _vswitch()
 	COMPREPLY=()
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts="panic protocol remove engage enable disengage disable disconnect status unpanic update ipversion host help who region location"
+	opts="panic protocol remove engage enable disengage disable connect disconnect status unpanic update ipversion host help who region location"
 
 	if [[ ${cur} == * ]] ; then
 		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
